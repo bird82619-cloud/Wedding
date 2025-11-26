995cd3d3971358d8f47d5e201606ec42dc2a68ff
Red <bird82619@gmail.com>
2025-11-26 15:50:17 +0000

    OK

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative paths for assets on GitHub Pages
+  base: '/wedding-rsvp/', // Ensures relative paths for assets on GitHub Pages
   build: {
     outDir: 'dist',
   },
